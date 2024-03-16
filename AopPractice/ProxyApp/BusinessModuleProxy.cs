b6d51705392f3909d047ba7c1f678c18168c1590
Burcu Tas <burcutas636@gmail.com>
2024-03-16 12:54:45 +0300

    Proxy Design Pattern

---
+++ AopPractice/ProxyApp/BusinessModuleProxy.cs
@@ -0,0 +1,19 @@
+﻿namespace ProxyApp
+{
+    public class BusinessModuleProxy : IBusinessModule
+    {
+        readonly IBusinessModule _module;
+
+        public BusinessModuleProxy(IBusinessModule module)
+        {
+            _module = module;
+        }
+
+        public void Method()
+        {
+            Console.WriteLine("Before method.");
+            _module.Method();
+            Console.WriteLine("After method.");
+        }
+    }
+}
---
+++ AopPractice/ProxyApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProxyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessModuleProxy businessModuleProxy = new(new BusinessModule());
+            businessModuleProxy.Method();
+        }
+    }
+}
